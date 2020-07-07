59b292b6991b59cca33eab6fe6a4439e31d5bc0f
Theresa <tmannschatz@gmail.com>
2020-07-07 17:07:10 +0200

    Restrict tracking stocks and add message badge - not working?

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def under_stock_limit?
+    stocks.count < 10
+  end
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol) # check if the stock already exists in db
+    return false unless stock # if it doesn't exist return false
+    stocks.where(id: stock.id).exists? # if the stock exists check if the stock is tracked by the user
+  end
+
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
 end
