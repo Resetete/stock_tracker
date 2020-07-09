a2460defbb0ea9eb2d77090a3e9e38baf3e5962b
Theresa <tmannschatz@gmail.com>
2020-07-09 13:02:52 +0200

    Add bug fixes to add friends dialog

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,14 +1,14 @@
 class FriendsController < ApplicationController
   def search
     if params[:friend].present?
-      @friend = User.where(first_name: params[:friend]) # .first
-      if @friend
+      @friend = User.where(first_name: params[:friend])
+      if @friend.any?
         respond_to do |format|
           format.js { render partial: 'users/friends_result' }
         end
       else
         respond_to do |format|
-          flash.now[:alert] = "We couldn't find your friend. Please enter an existing fiends name"
+          flash.now[:alert] = "We couldn't find your friend. Please enter an existing friends name"
           format.js { render partial: 'users/friends_result' }
         end
       end
