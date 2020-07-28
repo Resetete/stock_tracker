d402f64ce8f76433cfd82934b8ad801a2c9053ba
Theresa <tmannschatz@gmail.com>
2020-07-28 23:34:55 +0200

    Add wallet edit action

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,6 +1,7 @@
 class WalletsController < ApplicationController
-  before_action :set_wallet_entry, only: [:show]
+  before_action :set_wallet_entry, only: [:show, :edit, :update]
   before_action :require_same_user, only: [:show]
+  before_action :get_ticker_options, only: [:new, :edit]
 
   def index
   end
@@ -8,9 +9,20 @@ class WalletsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @wallet.update(wallet_params)
+      flash[:notice] = 'Wallet entry was successfully updated'
+      redirect_to @wallet
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @wallet = Wallet.new
-    @ticker_options = Wallet.get_ticker_options
   end
 
   def create
@@ -40,5 +52,9 @@ class WalletsController < ApplicationController
       flash[:alert] = 'You can only view wallet entries that you own.'
       redirect_to root_path
     end
+  end
+
+  def get_ticker_options
+    @ticker_options = Wallet.get_ticker_options
   end
 end
