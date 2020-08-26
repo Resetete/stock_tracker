66f002d4287340294188c88f4b882ac8fa6fd457
Theresa <tmannschatz@gmail.com>
2020-08-26 22:21:57 +0200

    Add calculate profit; Profit model

--- app/models/profit.rb
+++ app/models/profit.rb
@@ -1,37 +1,36 @@
 class Profit < ApplicationRecord
 
   def self.daily_profit
-    Wallet.distinct.pluck(:ticker).each do |ticker|
-      Wallet.where(buy_date: start_date..end_date && ticker == ticker).each_with_index do |wallet_entry, index|
-        # calculate for each entry and for a first empty bought_on date until today the total profit --> store in profits table
-        # for each of those wallet_entries calculate the profit for
-        profit = wallet_entry.calc_current_profit(wallet_entry.ticker, wallet_entry.amount_bought, wallet_entry.buy_price)
-        begin
-          create(ticker: wallet_entry.ticker, profit: profit, date: start_date+index)
-        rescue
-          flash[:alert] = "Something went wrong calculating the profits"
-        end
-      end
-    end
-    #@wallet.calc_current_profit(@wallet.ticker, @wallet.amount_bought, @wallet.buy_price)
-    #last_price = Stock.new_crypto_lookup(@wallet.ticker, @wallet.currency).last_price
-    #Profit.update(current_profit: updated_profit, last_price: last_price)
-  end
-
-  def self.test # fill_the_profit table
     tickers.each do |ticker|
       find_wallet_entries(ticker).each do |wallet_entry|
         date_range.each do |date|
-          #profit = wallet_entry.calc_current_profit(wallet_entry.ticker, wallet_entry.amount_bought, wallet_entry.buy_price) # needs to be passed the date for calculation!! If not the current course is used
-          profit = 9999
-          Profit.create(ticker: wallet_entry.ticker, profit: profit, date: date)
+          profit = calc_profit(wallet_entry.ticker, date, wallet_entry.amount_bought, wallet_entry.buy_price, wallet_entry.trading_fee, wallet_entry.selling_fee)
+          begin
+            Profit.create(ticker: wallet_entry.ticker, profit: profit, date: date)
+          rescue => e
+            p "Error occured: #{e}"
+            nil
+          end
         end
       end
     end
   end
 
-  def self.test2
-    call_crypto_api_historical('BTC', 'DateTime.new(2020, 1, 1, 10,10,10)')
+  def self.calc_profit(ticker, date, amount_bought, buy_price, trading_fee, selling_fee)
+    if call_crypto_api_historical(ticker, date).nil?
+      nil
+    else
+      profit = bought_crypto(amount_bought, buy_price) * call_crypto_api_historical(ticker, date) - amount_bought
+      if fees?(trading_fee, selling_fee)
+        if profit > 0
+          profit - fees(trading_fee, selling_fee)
+        else
+          profit + fees(trading_fee, selling_fee)
+        end
+      else
+        profit
+      end
+    end
   end
 
   def daily_total_profit
@@ -40,7 +39,7 @@ class Profit < ApplicationRecord
   private
 
   def self.start_date
-    DateTime.new(2020, 8, 1, 10, 10, 10) # should be the first bought_on where total profit in profits table is empty
+    DateTime.new(2020, 8, 23, 0, 0, 0) # should be the first bought_on where total profit in profits table is empty
   end
 
   def self.end_date
@@ -62,11 +61,27 @@ class Profit < ApplicationRecord
   def self.call_crypto_api_historical(ticker_symbol, timestamp, currency = 'EUR')
     # https://min-api.cryptocompare.com/documentation?key=Historical&cat=dataPriceHistorical
     # convert DateTime object to unixtime --> DateTime.new().to_time.to_i
-    api_url = URI.parse("https://min-api.cryptocompare.com/data/pricehistorical?fsym=#{ticker_symbol}&tsyms=#{currency}&ts=#{timestamp.to_time.to_i}")
-    response = Net::HTTP.get_response(api_url)
-    json_response = JSON.parse(response.body)[ticker_symbol][currency]
+    p timestamp
+    begin
+      api_url = URI.parse("https://min-api.cryptocompare.com/data/pricehistorical?fsym=#{ticker_symbol}&tsyms=#{currency}&ts=#{timestamp.to_time.to_i}")
+      response = Net::HTTP.get_response(api_url)
+      json_response = JSON.parse(response.body)[ticker_symbol][currency]
+    rescue => e
+      p "Error when fetching historical course: #{e}"
+      nil
+    end
   end
 
+  def self.bought_crypto(amount_bought, buy_price)
+    amount_bought / buy_price
+  end
 
+  def self.fees(trading_fee, selling_fee)
+    trading_fee.to_f + selling_fee.to_f
+  end
+
+  def self.fees?(trading_fee, selling_fee)
+    trading_fee.nil? || selling_fee.nil?
+  end
 
 end
